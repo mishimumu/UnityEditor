3171fa980aa6dc1b94283ab8fc94318d39b95b35
mishimumu <793978044@qq.com>
2018-05-22 16:55:54 +0800

    Create Attribute_RequireComponent.cs

---
+++ Assets/Script/Attribute_RequireComponent.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Attribute_RequireComponent : MonoBehaviour
+{
+    //自动给这个脚本添加依赖组件
+    //The RequireComponent attribute automatically adds required components as dependencies.
+}
+
