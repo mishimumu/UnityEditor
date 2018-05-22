d115d2f8769831f5fb00ceba7a501a9767769101
mishimumu <793978044@qq.com>
2018-05-22 15:59:16 +0800

    Create CustomEditor.cs

---
+++ Assets/Editor/CustomEditor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+[CustomEditor(typeof(CustomInspector))]
+public class CustomInspectorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+        CustomInspector customInspector = (CustomInspector)target;
+        EditorGUILayout.BeginVertical();
+        
+        customInspector.title=EditorGUILayout.TextField("title",customInspector.title);
+        EditorGUILayout.LabelField("内容：", customInspector.content);
+        customInspector.type=(MessageType)EditorGUILayout.EnumPopup("消息类型", customInspector.type);
+        customInspector.fontColor=EditorGUILayout.ColorField("字体颜色",customInspector.fontColor);
+        customInspector.font=EditorGUILayout.IntSlider("字体大小", customInspector.font, 10, 50);
+        customInspector.icon = EditorGUILayout.ObjectField("玩家头像", customInspector.icon, typeof(Texture2D), true, GUILayout.ExpandHeight(false)) as Texture2D;
+        EditorGUILayout.EndVertical();
+
+    }
+
+    private void OnSceneGUI()
+    {
+        
+    }
+
+    
+
+}
+
