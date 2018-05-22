f2894c38d8498f247ac78faa7c0b0183dc95314c
mishimumu <793978044@qq.com>
2018-05-22 15:45:51 +0800

    Create Attribute_AddComponentMenu.cs

---
+++ Assets/Script/Attribute_AddComponentMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[AddComponentMenu("MyComponent/Attribute_AddComponentMenu")]
+public class Attribute_AddComponentMenu : MonoBehaviour
+{
+    //快捷添加脚本
+    //The AddComponentMenu attribute allows you to place a script anywhere in the "Component" menu,
+    //instead of just the "Component->Scripts" menu.
+    //You use this to organize the Component menu better,
+    //this way improving workflow when adding scripts. Important notice: You need to restart.
+}
+
