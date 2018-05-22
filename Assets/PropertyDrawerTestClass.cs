4d7b2d87adf7aed7eb23eecae2334f6967f51dbf
mishimumu <793978044@qq.com>
2018-05-22 18:18:05 +0800

    Create PropertyDrawerTestClass.cs

---
+++ Assets/PropertyDrawerTestClass.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropertyDrawerTestClass : MonoBehaviour {
+
+    public Ingredient potionResult;
+    public Ingredient[] potionIngredients;
+}
