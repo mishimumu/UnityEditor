fa1452ed532556563739e65a555cec988456529c
mishimumu <793978044@qq.com>
2018-05-22 18:06:52 +0800

    Create Role.cs

---
+++ Assets/Script/Role.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Role
+{
+    public string id;
+    public string name;
+    public string desc;
+    public int attack;
+    public int def;
+    public int hp;
+    public string icon;
+    public string Audio;
+
+}
